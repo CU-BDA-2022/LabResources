734b290b2dd7202a8a3716badf6ac5963e373e64
Tom Loredo <loredo@astro.cornell.edu>
2022-04-13 14:28:54 -0400

    Added PyStan-2 test file

---
+++ PyStan-2/PyStanTest.py
@@ -0,0 +1,23 @@
+import pystan
+
+# Stan code for a trivial model---a normal prior for
+# theta, with no data or likelihood function.
+model_code = '''
+    parameters {real theta;}
+    model {theta ~ normal(0,1);}
+'''
+
+# The following will invoke Stan to build and compile a C++
+# library; it will take some time and report progress to
+# the console.
+model = pystan.StanModel(model_code=model_code)
+
+# This will run an MCMC algorithm for 2000 steps, discarding
+# the first half of the run as burn-in; it will report progress
+# to the console.
+results = model.sampling(n_jobs=1)
+
+# Here we print a Monte Carlo estimate of the posterior mean
+# for y; if all goes well it should be near 0.
+thetas = results.extract()['theta']
+print('Mean of posterior samples:', thetas.mean())
